d035f69d5ae7e7a34ea952bf970bf7221570820b
Gabriel Gutierrez <gg447062@gmail.com>
2022-03-31 19:44:56 -0600

    updates api to also return number of locked samples

--- server/api/floppys.js
+++ server/api/floppys.js
@@ -7,9 +7,17 @@ router.get('/:id', async (req, res, next) => {
     const query = new Moralis.Query('Sample');
     query.equalTo('unlocked', true);
     query.equalTo('floppy', parseInt(req.params.id));
-    const singleFloppySamples = await query.find();
 
-    res.send(singleFloppySamples);
+    const query2 = new Moralis.Query('Sample');
+    query2.equalTo('unlocked', false);
+    query2.equalTo('floppy', parseInt(req.params.id));
+
+    const unlockedSamples = await query.find();
+    const lockedSamples = await query2.find();
+
+    const result = { locked: lockedSamples.length, unlocked: unlockedSamples };
+
+    res.send(result);
   } catch (error) {
     next(error);
   }
@@ -27,11 +35,16 @@ router.put('/:id', async (req, res, next) => {
       await locked[0].set('unlocked', true);
       await locked[0].save();
     }
+
     const secondQuery = new Moralis.Query('Sample');
     secondQuery.equalTo('unlocked', true);
     secondQuery.equalTo('floppy', parseInt(req.params.id));
-    const unlocked = await secondQuery.find();
-    res.send(unlocked);
+    const unlockedSamples = await secondQuery.find();
+    const lockedSamplesCount = locked.length ? locked.length - 1 : 0;
+
+    const result = { locked: lockedSamplesCount, unlocked: unlockedSamples };
+
+    res.send(result);
   } catch (error) {
     next(error);
   }
