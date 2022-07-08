cdd6fadfca978317baf996b91d6d9ca310beedee
Gabriel Gutierrez <gg447062@gmail.com>
2022-07-08 13:51:53 -0500

    saves req body to db

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = require('./server');
 const dotenv = require('dotenv').config();
 const serverUrl = process.env.TEST_SERVER_URL;
 const appId = process.env.TEST_APP_ID;
+// const masterKey = process.env.TEST_MASTER_KEY;
 
 const PORT = process.env.PORT || 3000;
 
--- server/api/uploads.js
+++ server/api/uploads.js
@@ -1,21 +1,36 @@
 const router = require('express').Router();
 const Moralis = require('moralis/node');
 
-// POST /api/uploads
-router.post('/', async (req, res, next) => {
+// POST /api/uploads/:name
+router.post('/:name', async (req, res, next) => {
   try {
+    const name = req.params.name;
+    const body = req.body.song;
+    const body64 = JSON.stringify(btoa(body));
+    // const audioFile = new Moralis.File(`${name}.wav`, {
+    //   base64: body64,
+    // });
+    // await audioFile.saveIPFS();
+    // const audioHash = audioFile.hash();
+    // console.log(audioHash);
     const Upload = Moralis.Object.extend('Upload');
     const upload = new Upload();
     upload.set('time', new Date().toISOString());
+    upload.set('name', name);
+    upload.set('base64OG', body);
+    upload.set('base64Converted', body64);
     upload.save().then(
       (upload) => {
+        // console.log(upload.get(id));
         res.send('ok');
       },
       (error) => {
+        console.log(error);
         res.send('error');
       }
     );
   } catch (error) {
+    console.log(error);
     next(error);
   }
 });
