99de2442d9379f129a12daeb69570885f681cd38
Gabriel Gutierrez <gg447062@gmail.com>
2022-08-10 12:28:25 -0500

    updates upload and download methods

--- server/api/downloads.js
+++ server/api/downloads.js
@@ -2,14 +2,17 @@ const router = require('express').Router();
 const Moralis = require('moralis/node');
 
 // POST /api/downloads/:name
-router.post('/:name', async (req, res, next) => {
+router.post('/:name/:address', async (req, res, next) => {
   try {
     const name = req.params.name;
+    const address = req.params.address;
     const body = req.body.song;
+    console.log(body);
     const body64 = Buffer.from(body).toString('base64');
     const Download = Moralis.Object.extend('Download');
     const download = new Download();
     download.set('time', new Date().toISOString());
+    download.set('address', address);
     download.set('name', name);
     download.set('base64OG', body);
     download.set('base64Converted', body64);
--- server/api/uploads.js
+++ server/api/uploads.js
@@ -2,20 +2,22 @@ const router = require('express').Router();
 const Moralis = require('moralis/node');
 
 // POST /api/uploads/:name
-router.post('/:name', async (req, res, next) => {
+router.post('/:name/:address', async (req, res, next) => {
   try {
     const name = req.params.name;
+    const address = req.params.address;
     const body = req.body.song;
+    console.log(body);
     const body64 = Buffer.from(body).toString('base64');
     const Upload = Moralis.Object.extend('Upload');
     const upload = new Upload();
     upload.set('time', new Date().toISOString());
+    upload.set('address', address);
     upload.set('name', name);
     upload.set('base64OG', body);
     upload.set('base64Converted', body64);
     upload.save().then(
       (upload) => {
-        // console.log(upload.get(id));
         res.send('ok');
       },
       (error) => {
