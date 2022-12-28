c761547cd6c203446b9b009e8bb5461a3b53035c
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2022-12-28 12:28:36 -0700

    save to ipfs instead of firebase storage

--- server/api/uploads.js
+++ server/api/uploads.js
@@ -1,19 +1,15 @@
+const axios = require('axios')
 const router = require('express').Router();
-const { db, bucket } = require('../../firebase');
-const stream = require('stream');
+const { db } = require('../../firebase');
 
 // POST /api/:download flag
 router.post('/:download', async (req, res, next) => {
   try {
-    const ip = req.headers['x-forwarded-for'];
+    const ip = req.headers['x-forwarded-for']
     const download = req.params.download === '1' ? true : false
-    
     const bodyParts = req.body.song.split('#')
     const name = bodyParts[1];
-    
     const repaired = bodyParts[0].split(' ').join('+');
-
-    const contentString = `data:audio/wav;base64, ${repaired}`
 
     const options = {
       method: 'POST',
@@ -25,17 +21,18 @@ router.post('/:download', async (req, res, next) => {
       },
       data: [
         {
-          content: contentString,
           path: `${name}.wav`,
+          content: repaired,
         },
       ],
+      maxBodyLength: Infinity
     };
-    
+
     const { data } = await axios.request(options);
-    const hash = data[0].path;
+    const hash = data[0].path.split('/ipfs/')[1]
 
     await db.collection('uploads').add({
-      time: new Date().toLocaleString(),
+      timestamp: Date.now(),
       ip: ip,
       name: name,
       hash: hash,
@@ -44,31 +41,9 @@ router.post('/:download', async (req, res, next) => {
 
     res.send('ok');
   } catch (error) {
-    console.log(error);
+    // console.log(error.message);
     next(error);
   }
 });
-
-async function uploadToIPFS(path, content){
- 
-    const options = {
-      method: 'POST',
-      url: 'https://deep-index.moralis.io/api/v2/ipfs/uploadFolder',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        'X-API-Key': process.env.MORALIS_API_KEY,
-      },
-      data: [
-        {
-          content,
-          path,
-        },
-      ],
-    };
-    const { data } = await axios.request(options);
-    const hash = data[0].path;
-    return hash
-}
 
 module.exports = router;
