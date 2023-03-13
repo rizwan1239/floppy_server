832594cacfa008c9d486b818169097b5e66711f7
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-03-13 15:20:34 -0600

    annoyingly add console.log

--- server/api/uploads.js
+++ server/api/uploads.js
@@ -11,6 +11,7 @@ router.post('/:download', async (req, res, next) => {
     // const name = bodyParts[1];
     // const repaired = bodyParts[0].split(' ').join('+');
     const name = req.body.path;
+    console.log(req.body);
     const repaired = req.body.content['base64File'].split(' ').join('+');
 
     const options = {
