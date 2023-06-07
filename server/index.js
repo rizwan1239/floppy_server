3047a857b449c707b5a95cf21adcbbaff05f0338
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-06-07 12:26:40 -0600

    adds cors middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 //logging middleware
 app.use(morgan('dev'));
