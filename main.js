74a87861ae9d4caa3a1b90175d5df18240f7e05e
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-07-12 17:46:05 -0600

    adds socket.io

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-console */
 const app = require('./server');
 const dotenv = require('dotenv').config();
+const socket = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('new connection:', socket.id);
+
+  socket.on('disconnect', (reason) => {
+    console.log('socket disconnected:', reason);
+  });
+  socket.emit('message', 'welcome');
 });
