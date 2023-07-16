becb7b72ab840951053181f1fab1f5612994a824
Muhammad Rizwan Babar <33382155+rizwan1239@users.noreply.github.com>
2023-07-16 21:34:01 +0100

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -1,15 +1,15 @@
-//const { initializeApp, cert } = require('firebase-admin/app');
-//const { getFirestore } = require('firebase-admin/firestore');
-//const { getStorage } = require('firebase-admin/storage');
-//const dotenv = require('dotenv').config();
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const { getStorage } = require('firebase-admin/storage');
+const dotenv = require('dotenv').config();
 
-//const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 
-//initializeApp({
-//  credential: cert(serviceAccount),
-//  storageBucket: 'floppy-2bc4b.appspot.com',
-//});
-//const bucket = getStorage().bucket();
-//const db = getFirestore();
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: 'floppy-2bc4b.appspot.com',
+});
+const bucket = getStorage().bucket();
+const db = getFirestore();
 
-//module.exports = { db, bucket };
+module.exports = { db, bucket };
